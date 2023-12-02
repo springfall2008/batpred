8c372043db38f8c68312507bea1b0f59f3e607b8
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2023-12-02 21:25:31 +0000

    Debug print typo

--- apps/predbat/predbat.py
+++ apps/predbat/predbat.py
@@ -8043,12 +8043,12 @@ class PredBat(hass.Hass):
 
                 # Avoid having too long a period to configure as registers only support 24-hours
                 if (minutes_start < self.minutes_now) and ((minutes_end - minutes_start) >= 24*60):
+                    minutes_start = int(self.minutes_now / 30) * 30
                     self.log(
                         "Move on window start time to avoid wrap - new start {}".format(
                             self.time_abs_str(minutes_start)
                         )
                     )
-                    minutes_start = int(self.minutes_now / 30) * 30
 
                 # Check if end is within 24 hours of now and end is in the future
                 if (minutes_end - self.minutes_now) < 24 * 60 and minutes_end > self.minutes_now:
