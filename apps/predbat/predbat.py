b77a9afc81e62f5a1c0de706675bb318c48969ca
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2023-12-23 13:44:30 +0000

    Tweaks to inverter reset to avoids resets in read-only mode (#519)
    * Tweaks to inverter reset to avoids resets in read-only mode
    
    ---------
    
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- apps/predbat/predbat.py
+++ apps/predbat/predbat.py
@@ -399,7 +399,7 @@ CONFIG_ITEMS = [
     {"name": "set_reserve_enable", "friendly_name": "Set Reserve Enable", "type": "switch", "enable": "expert_mode", "default": True, "reset_inverter": True},
     {"name": "set_discharge_freeze_only", "friendly_name": "Set Discharge Freeze Only", "type": "switch", "enable": "expert_mode", "default": False, "reset_inverter": True},
     {"name": "set_discharge_during_charge", "friendly_name": "Set Discharge During Charge", "type": "switch", "default": True},
-    {"name": "set_read_only", "friendly_name": "Read Only mode", "type": "switch", "default": False, "reset_inverter": True},
+    {"name": "set_read_only", "friendly_name": "Read Only mode", "type": "switch", "default": False, "reset_inverter_force": True},
     {"name": "balance_inverters_enable", "friendly_name": "Balance Inverters Enable (Beta)", "type": "switch", "default": False},
     {"name": "balance_inverters_charge", "friendly_name": "Balance Inverters for charging", "type": "switch", "enable": "balance_inverters_enable", "default": True},
     {"name": "balance_inverters_discharge", "friendly_name": "Balance Inverters for discharge", "type": "switch", "enable": "balance_inverters_enable", "default": True},
@@ -6216,6 +6216,7 @@ class PredBat(hass.Hass):
         Init stub
         """
         self.inverter_needs_reset = False
+        self.inverter_needs_reset_force = False
         self.config_index = {}
         self.dashboard_index = []
         self.prefix = self.args.get("prefix", "predbat")
@@ -8449,15 +8450,18 @@ class PredBat(hass.Hass):
         """
         Reset inverter to safe mode
         """
-        for inverter in self.inverters:
-            self.log("Reset inverter settings to safe mode")
-            inverter.adjust_charge_rate(inverter.battery_rate_max_charge * 60.0 * 1000.0)
-            inverter.adjust_discharge_rate(inverter.battery_rate_max_discharge * 60 * 1000)
-            inverter.adjust_force_discharge(False)
-            inverter.adjust_reserve(0)
-            inverter.adjust_battery_target(100.0)
-            inverter.disable_charge_window()
+        if not self.set_read_only or self.inverter_needs_reset_force:
+            # Don't reset in read only mode unless forced
+            for inverter in self.inverters:
+                self.log("Reset inverter settings to safe mode")
+                inverter.adjust_charge_rate(inverter.battery_rate_max_charge * 60.0 * 1000.0)
+                inverter.adjust_discharge_rate(inverter.battery_rate_max_discharge * 60 * 1000)
+                inverter.adjust_force_discharge(False)
+                inverter.adjust_reserve(0)
+                inverter.adjust_battery_target(100.0)
+                inverter.disable_charge_window()
         self.inverter_needs_reset = False
+        self.inverter_needs_reset_force = False
 
     def execute_plan(self):
         status_extra = ""
@@ -9641,7 +9645,7 @@ class PredBat(hass.Hass):
             if ("entity" in item) and (item["entity"] in entities):
                 entity = item["entity"]
                 self.log("select_event: {} = {}".format(entity, value))
-                self.expose_config(item["name"], value)
+                self.expose_config(item["name"], value, event=True)
                 self.update_pending = True
                 self.plan_valid = False
 
@@ -9661,7 +9665,7 @@ class PredBat(hass.Hass):
             if ("entity" in item) and (item["entity"] in entities):
                 entity = item["entity"]
                 self.log("number_event: {} = {}".format(entity, value))
-                self.expose_config(item["name"], value)
+                self.expose_config(item["name"], value, event=True)
                 self.update_pending = True
                 self.plan_valid = False
 
@@ -9690,7 +9694,7 @@ class PredBat(hass.Hass):
                     value = not value
 
                 self.log("switch_event: {} = {}".format(entity, value))
-                self.expose_config(item["name"], value)
+                self.expose_config(item["name"], value, event=True)
                 self.update_pending = True
                 self.plan_valid = False
 
@@ -9709,7 +9713,7 @@ class PredBat(hass.Hass):
                 return value, default
         return None, default
 
-    def expose_config(self, name, value, quiet=True):
+    def expose_config(self, name, value, quiet=True, event=False):
         """
         Share the config with HA
         """
@@ -9724,9 +9728,13 @@ class PredBat(hass.Hass):
                 if entity and ((item.get("value") is None) or (value != item["value"])):
                     if item.get("reset_inverter", False):
                         self.inverter_needs_reset = True
+                    if item.get("reset_inverter_force", False):
+                        self.inverter_needs_reset = True
+                        if event:
+                            self.inverter_needs_reset_force = True
                     item["value"] = value
                     if not quiet:
-                        self.log("Updating HA config {} to {} inverter_needs_reset {}".format(name, value, self.inverter_needs_reset))
+                        self.log("Updating HA config {} to {}".format(name, value))
                     if item["type"] == "input_number":
                         icon = item.get("icon", "mdi:numeric")
                         self.set_state(
