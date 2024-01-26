ce19a0605af0047fab2d63b3e9855bb72541556d
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2024-01-26 21:04:18 +0000

    Fix issue with charge rate being reset when it shouldn't be in low power mode (#652)
    * Fix issue with charge rate being reset when it shouldn't be in low power mode
    
    Maybe related to:
    
    https://github.com/springfall2008/batpred/issues/650
    
    
    ---------
    
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- apps/predbat/predbat.py
+++ apps/predbat/predbat.py
@@ -9101,7 +9101,7 @@ class PredBat(hass.Hass):
                         inverter.adjust_force_discharge(False)
                         resetReserve = True
 
-                    if self.set_discharge_freeze:
+                    if self.set_discharge_freeze and not isCharging:
                         # In discharge freeze mode we disable charging during discharge slots, so turn it back on otherwise
                         inverter.adjust_charge_rate(inverter.battery_rate_max_charge * 60 * 1000)
             elif self.set_discharge_window:
