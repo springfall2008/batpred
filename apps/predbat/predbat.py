916a3bce3c37bcd67bb0357e95ce76282e235b6c
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2024-02-09 19:39:53 +0000

    Fix reading of nordpool plugin data to include tomorrow (#743)
    * Fix reading of nordpool plugin data to include tomorrow
    
    https://github.com/springfall2008/batpred/issues/718
    
    * Debug line removed
    
    * [pre-commit.ci lite] apply automatic fixes
    
    ---------
    
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- apps/predbat/predbat.py
+++ apps/predbat/predbat.py
@@ -21,7 +21,7 @@ import pytz
 import requests
 import yaml
 
-THIS_VERSION = "v7.15.18"
+THIS_VERSION = "v7.15.19"
 PREDBAT_FILES = ["predbat.py"]
 TIME_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
 TIME_FORMAT_SECONDS = "%Y-%m-%dT%H:%M:%S.%f%z"
@@ -10212,6 +10212,11 @@ class PredBat(hass.Hass):
                     data_import = self.get_state(entity_id=next_rate_id, attribute="all_rates")
                     if data_import:
                         data_all += data_import
+            else:
+                # Nordpool tomorrow
+                data_import = self.get_state(entity_id=current_rate_id, attribute="raw_tomorrow")
+                if data_import:
+                    data_all += data_import
 
         if data_all:
             rate_key = "rate"
@@ -11498,7 +11503,7 @@ class PredBat(hass.Hass):
                                 "friendly_name": item["friendly_name"],
                                 "title": item["title"],
                                 "in_progress": in_progress,
-                                "auto_update": True,
+                                "auto_update": False,
                                 "installed_version": item["installed_version"],
                                 "latest_version": latest,
                                 "entity_picture": item["entity_picture"],
