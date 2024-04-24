2c800ee87f2163f5306c6007bbd5282931a1c34d
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2024-04-24 16:46:29 +0100

    Fix for crash with cloud model disabled, Solis inverter fix
    * Fix for crash with cloud model disabled
    * Possible fix for discharge start time overlap issue: https://github.com/springfall2008/batpred/issues/962

--- apps/predbat/predbat.py
+++ apps/predbat/predbat.py
@@ -27,7 +27,7 @@ from multiprocessing import Pool, cpu_count
 if not "PRED_GLOBAL" in globals():
     PRED_GLOBAL = {}
 
-THIS_VERSION = "v7.17.1"
+THIS_VERSION = "v7.17.2"
 PREDBAT_FILES = ["predbat.py"]
 TIME_FORMAT = "%Y-%m-%dT%H:%M:%S%z"
 TIME_FORMAT_SECONDS = "%Y-%m-%dT%H:%M:%S.%f%z"
@@ -4855,7 +4855,7 @@ class PredBat(hass.Hass):
         """
         Download one or more entities for import/export data
         """
-        if "." not in key:
+        if '.' not in key:
             entity_ids = self.get_arg(key, indirect=False)
         else:
             entity_ids = key
@@ -7782,9 +7782,7 @@ class PredBat(hass.Hass):
         plan_debug = self.get_arg("plan_debug")
         html = "<table>"
         html += "<tr>"
-        html += "<td colspan=10> Plan starts: {} last updated: {} version: {}</td>".format(
-            self.now_utc.strftime("%Y-%m-%d %H:%M"), self.now_utc_real.strftime("%H:%M:%S"), THIS_VERSION
-        )
+        html += "<td colspan=10> Plan starts: {} last updated: {} version: {}</td>".format(self.now_utc.strftime("%Y-%m-%d %H:%M"), self.now_utc_real.strftime("%H:%M:%S"), THIS_VERSION)
         html += "</tr>"
         html += self.get_html_plan_header(plan_debug)
         minute_now_align = int(self.minutes_now / 30) * 30
@@ -10938,10 +10936,10 @@ class PredBat(hass.Hass):
             type_load=True,
             load_forecast=self.load_forecast,
             load_scaling_dynamic=self.load_scaling_dynamic,
-            cloud_factor=min(self.metric_load_divergence + 0.5, 1.0),
+            cloud_factor=min(self.metric_load_divergence + 0.5, 1.0) if self.metric_load_divergence else None,
         )
         pv_forecast_minute_step = self.step_data_history(self.pv_forecast_minute, self.minutes_now, forward=True, cloud_factor=self.metric_cloud_coverage)
-        pv_forecast_minute10_step = self.step_data_history(self.pv_forecast_minute10, self.minutes_now, forward=True, cloud_factor=min(self.metric_cloud_coverage + 0.2, 1.0))
+        pv_forecast_minute10_step = self.step_data_history(self.pv_forecast_minute10, self.minutes_now, forward=True, cloud_factor=min(self.metric_cloud_coverage + 0.2, 1.0) if self.metric_cloud_coverage else None)
 
         # Save step data for debug
         if self.debug_enable:
@@ -10949,7 +10947,7 @@ class PredBat(hass.Hass):
             self.load_minutes_step10 = load_minutes_step10
             self.pv_forecast_minute_step = pv_forecast_minute_step
             self.pv_forecast_minute10_step = pv_forecast_minute10_step
-
+    
         # Creation prediction object
         self.prediction = Prediction(self, pv_forecast_minute_step, pv_forecast_minute10_step, load_minutes_step, load_minutes_step10)
 
@@ -11378,12 +11376,12 @@ class PredBat(hass.Hass):
                 if (inverter.discharge_start_time_minutes < self.minutes_now) and (self.minutes_now >= minutes_start):
                     minutes_start = inverter.discharge_start_time_minutes
                     # Don't allow overlap with charge window
-                    if minutes_start >= inverter.charge_start_time_minutes and minutes_start < inverter.charge_end_time_minutes:
+                    if minutes_start < inverter.charge_end_time_minutes and minutes_end >= inverter.charge_start_time_minutes:
                         minutes_start = window["start"]
                     else:
                         self.log(
-                            "Include original discharge start {} with our start which is {}".format(
-                                self.time_abs_str(inverter.discharge_start_time_minutes), self.time_abs_str(minutes_start)
+                            "Include original discharge start {} with our start which is {} (charge start {} end {})".format(
+                                self.time_abs_str(inverter.discharge_start_time_minutes), self.time_abs_str(minutes_start), self.time_abs_str(inverter.charge_start_time_minutes),  self.time_abs_str(inverter.charge_end_time_minutes)
                             )
                         )
 
