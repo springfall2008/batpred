a94bb7d535b7d7f91879b5f5685b16f5b5fab80a
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2024-05-26 16:41:16 +0100

    Watching/restarting for standalone version

--- apps/predbat/hass.py
+++ apps/predbat/hass.py
@@ -13,10 +13,23 @@ from aiohttp import web, ClientSession, WSMsgType
 import threading
 import os
 
+def check_modified(py_files, start_time):
+    """
+    Check if .py file was changed since we started
+    """
+
+    # Check the last modified timestamp of each .py file
+    for file_path in py_files:
+        last_modified = os.path.getmtime(file_path)
+        last_modified_timestamp = datetime.fromtimestamp(last_modified)
+        if last_modified_timestamp > start_time:
+            return True
+    return False
 
 async def main():
     print("**** Starting Standalone Predbat ****")
-
+    start_time = datetime.now()
+   
     try:
         p_han = predbat.PredBat()
         p_han.initialize()
@@ -24,17 +37,27 @@ async def main():
         print("Error: Failed to start predbat {}".format(e))
         return
 
+    # Find all .py files in the directory hierarchy
+    py_files = []
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            if file.endswith('.py'):
+                py_files.append(os.path.join(root, file))
+    print("Watching {} for changes".format(py_files))
+
     # Runtime loop
     while True:
         time.sleep(1)
         await p_han.timer_tick()
+        if check_modified(py_files, start_time):
+            print("Stopping Predbat due to file changes....")
+            await p_han.stop_all()
+            break
 
-
-if __name__ == "__main__":
-    set_start_method("fork")
+if __name__ == '__main__':
+    set_start_method('fork')   
     asyncio.run(main())
     sys.exit(0)
-
 
 class Hass:
     def log(self, msg, quiet=True):
@@ -45,12 +68,12 @@ class Hass:
         self.logfile.write(message)
         self.logfile.flush()
         if not quiet:
-            print(message, end="")
+            print(message, end='')
         log_size = self.logfile.tell()
         if log_size > 10000000:
             self.logfile.close()
-            os.rename("predbat.log", "predbat.log.1")
-            self.logfile = open("predbat.log", "w")
+            os.rename('predbat.log', 'predbat.log.1')
+            self.logfile = open('predbat.log', 'w')
 
     async def run_in_executor(self, callback, *args):
         """
@@ -79,31 +102,44 @@ class Hass:
         self.log("Creating task: {}".format(task), quiet=False)
         t1 = threading.Thread(name="TaskCreate", target=self.task_waiter, args=[task])
         t1.start()
+        self.threads.append(t1)
         return t1
 
+    async def stop_all(self):
+        """
+        Stop Predbat
+        """
+        self.log("Stopping Predbat", quiet=False)
+        await self.terminate()
+
+        for t in self.threads:
+            t.join()
+        self.logfile.close()
+        
     def __init__(self):
         """
         Start Predbat
         """
         self.args = {}
         self.run_list = []
+        self.threads = []
 
-        self.logfile = open("predbat.log", "a")
+        self.logfile = open('predbat.log', 'a')
 
         # Open YAML file apps.yaml and read it
         self.log("Loading apps.yaml", quiet=False)
-        with io.open("apps.yaml", "r") as stream:
+        with io.open('apps.yaml', 'r') as stream:
             try:
                 config = yaml.safe_load(stream)
-                self.args = config["pred_bat"]
+                self.args = config['pred_bat']
             except yaml.YAMLError as exc:
                 print(exc)
                 sys.exit(1)
 
-        if "ha_url" not in self.args:
+        if 'ha_url' not in self.args:
             print("Error: ha_url not found in apps.yaml")
             sys.exit(1)
-        if "ha_key" not in self.args:
+        if 'ha_key' not in self.args:
             print("Error: ha_key not found in apps.yaml")
             sys.exit(1)
 
@@ -111,18 +147,18 @@ class Hass:
         """
         Run a function every x seconds
         """
-        self.run_list.append({"callback": callback, "next_time": next_time, "run_every": run_every, "kwargs": kwargs})
+        self.run_list.append({'callback': callback, 'next_time': next_time, 'run_every': run_every, 'kwargs': kwargs})
         return True
-
+    
     async def timer_tick(self):
         """
         Timer tick function, executes tasks at the correct time
         """
         now = datetime.now()
         for item in self.run_list:
-            if now > item["next_time"]:
-                self.log("Running task: {}".format(item["callback"]), quiet=False)
-                item["callback"](None)
-                while now > item["next_time"]:
-                    run_every = timedelta(seconds=item["run_every"])
-                    item["next_time"] += run_every
+            if now > item['next_time']:
+                self.log("Running task: {}".format(item['callback']), quiet=False)
+                item['callback'](None)
+                while now > item['next_time']:
+                    run_every = timedelta(seconds=item['run_every'])
+                    item['next_time'] += run_every
