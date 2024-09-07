e61b86587222cd42dbc4c2e573e6e99013e4ec69
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2024-09-07 14:54:03 +0100

    Web charts started work (#1419)
    * Web charts started work
    
    * [pre-commit.ci lite] apply automatic fixes
    
    * Tidy
    
    * [pre-commit.ci lite] apply automatic fixes
    
    * Retain last web page, chart style fixes
    
    * [pre-commit.ci lite] apply automatic fixes
    
    * Tidy up chart
    
    * [pre-commit.ci lite] apply automatic fixes
    
    ---------
    
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- apps/predbat/web.py
+++ apps/predbat/web.py
@@ -4,9 +4,11 @@ from aiohttp import web
 import asyncio
 import os
 import re
+from datetime import datetime, timedelta
 
 from config import CONFIG_ITEMS
 from utils import calc_percent_limit
+from config import TIME_FORMAT
 
 
 class WebInterface:
@@ -14,6 +16,7 @@ class WebInterface:
         self.abort = False
         self.base = base
         self.log = base.log
+        self.default_page = "./dash"
 
     async def start(self):
         # Start the web server on port 5052
@@ -23,6 +26,7 @@ class WebInterface:
         app.router.add_get("/log", self.html_log)
         app.router.add_get("/menu", self.html_menu)
         app.router.add_get("/apps", self.html_apps)
+        app.router.add_get("/charts", self.html_charts)
         app.router.add_get("/config", self.html_config)
         app.router.add_post("/config", self.html_config_post)
         app.router.add_get("/dash", self.html_dash)
@@ -58,6 +62,11 @@ class WebInterface:
         text += "</table>"
         return text
 
+    def icon2html(self, icon):
+        if icon:
+            icon = '<span class="mdi mdi-{}"></span>'.format(icon.replace("mdi:", ""))
+        return icon
+
     def get_status_html(self, level, status):
         text = ""
         if not self.base.dashboard_index:
@@ -73,15 +82,13 @@ class WebInterface:
 
         text += "<h2>Entities</h2>\n"
         text += "<table>\n"
-        text += "<tr><th>Icon</th><th>Name</th><th>Entity</th><th>State</th><th>Attributes</th></tr>\n"
+        text += "<tr><th></th><th>Name</th><th>Entity</th><th>State</th><th>Attributes</th></tr>\n"
 
         for entity in self.base.dashboard_index:
             state = self.base.dashboard_values.get(entity, {}).get("state", None)
             attributes = self.base.dashboard_values.get(entity, {}).get("attributes", {})
             unit_of_measurement = attributes.get("unit_of_measurement", "")
-            icon = attributes.get("icon", "")
-            if icon:
-                icon = '<span class="mdi mdi-{}"></span>'.format(icon.replace("mdi:", ""))
+            icon = self.icon2html(attributes.get("icon", ""))
             if unit_of_measurement is None:
                 unit_of_measurement = ""
             friendly_name = attributes.get("friendly_name", "")
@@ -102,6 +109,7 @@ class WebInterface:
 
         text += """
 <link href="https://cdn.jsdelivr.net/npm/@mdi/font@7.4.47/css/materialdesignicons.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 <style>
         body, html {
             margin: 0;
@@ -169,10 +177,106 @@ class WebInterface:
         text += "</head>\n"
         return text
 
+    def get_entity_results(self, entity):
+        results = self.base.dashboard_values.get(entity, {}).get("attributes", {}).get("results", {})
+        return results
+
+    def get_chart_series(self, name, results, chart_type="line"):
+        """
+        Return HTML for a chart series
+        """
+        text = ""
+        text += "   {\n"
+        text += "    name: '{}',\n".format(name)
+        text += "    type: '{}',\n".format(chart_type)
+        text += "    data: [\n"
+        first = True
+        for key in results:
+            if not first:
+                text += ","
+            first = False
+            text += "{"
+            text += "x: new Date('{}').getTime(),".format(key)
+            text += "y: {}".format(results[key])
+            text += "}"
+        text += "  ]\n"
+        text += "  }\n"
+        return text
+
+    def get_chart(self):
+        """
+        Return the HTML for a chart
+        """
+        now_str = self.base.now_utc.strftime(TIME_FORMAT)
+        soc_kw_h0 = {}
+        if self.base.soc_kwh_history:
+            hist = self.base.soc_kwh_history
+            for minute in range(0, self.base.minutes_now, 15):
+                minute_timestamp = self.base.midnight_utc + timedelta(minutes=minute)
+                stamp = minute_timestamp.strftime(TIME_FORMAT)
+                soc_kw_h0[stamp] = hist.get(self.base.minutes_now - minute, 0)
+        soc_kw_h0[now_str] = self.base.soc_kw
+
+        soc_kw_best = self.get_entity_results("predbat.soc_kw_best")
+        best_charge_limit_kw = self.get_entity_results("predbat.best_charge_limit_kw")
+        best_discharge_limit_kw = self.get_entity_results("predbat.best_discharge_limit_kw")
+        text = ""
+        if soc_kw_best:
+            text += "<script>\n"
+            text += "var options = {\n"
+            text += "  chart: {\n"
+            text += "    type: 'line',\n"
+            text += "    width: '80%'\n"
+            text += "  },\n"
+            text += "  span: {\n"
+            text += "    start: 'minute', offset: '-12h'\n"
+            text += "  },\n"
+            text += "  series: [\n"
+            text += self.get_chart_series("soc_best", soc_kw_best)
+            text += "  ,\n"
+            text += self.get_chart_series("soc_actual", soc_kw_h0)
+            text += "  ,\n"
+            text += self.get_chart_series("charge_limit", best_charge_limit_kw, chart_type="area")
+            text += "  ,\n"
+            text += self.get_chart_series("discharge_limit", best_discharge_limit_kw)
+            text += "  ],\n"
+            text += "  fill: {\n"
+            text += "     opacity: [1.0, 1.0, 0.5, 1.0]\n"
+            text += "  },\n"
+            text += "  stroke: {\n"
+            text += "     width: [1, 1, 4, 2],\n"
+            text += "     curve: ['smooth', 'smooth', 'stepline', 'stepline'],\n"
+            text += "  },\n"
+            text += "  xaxis: {\n"
+            text += "    type: 'datetime'\n"
+            text += "  },\n"
+            text += "  yaxis: {\n"
+            text += "    title: {text: 'kWh'}\n"
+            text += "  },\n"
+            text += "  title: {\n"
+            text += "    text: 'Predbat Battery Chart'\n"
+            text += "  },\n"
+            text += "  annotations: {\n"
+            text += "   xaxis: [\n"
+            text += "    {\n"
+            text += "       x: new Date('{}').getTime(),\n".format(now_str)
+            text += "       borderColor: '#775DD0'\n"
+            text += "    }\n"
+            text += "   ]\n"
+            text += "  }\n"
+            text += "}\n"
+            text += "var chart = new ApexCharts(document.querySelector('#chart'), options);\n"
+            text += "chart.render();\n"
+            text += "</script>\n"
+        else:
+            text += "<h2>Loading...</h2>"
+        return text
+
     async def html_plan(self, request):
         """
         Return the Predbat plan as an HTML page
         """
+        self.default_page = "./plan"
         html_plan = self.base.html_plan
         text = self.get_header("Predbat Plan", refresh=60)
         text += "<body>{}</body></html>\n".format(html_plan)
@@ -184,6 +288,7 @@ class WebInterface:
         """
         logfile = "predbat.log"
         logdata = ""
+        self.default_page = "./log"
         if os.path.exists(logfile):
             with open(logfile, "r") as f:
                 logdata = f.read()
@@ -194,8 +299,10 @@ class WebInterface:
         warnings = False
         if "errors" in args:
             errors = True
+            self.default_page = "./log?errors"
         if "warnings" in args:
             warnings = True
+            self.default_page = "./log?warnings"
 
         loglines = logdata.split("\n")
         text = self.get_header("Predbat Log", refresh=10)
@@ -335,6 +442,7 @@ class WebInterface:
         """
         Render apps.yaml as an HTML page
         """
+        self.default_page = "./dash"
         text = self.get_header("Predbat Dashboard")
         text += "<body>\n"
         soc_perc = calc_percent_limit(self.base.soc_kw, self.base.soc_max)
@@ -342,10 +450,23 @@ class WebInterface:
         text += "</body></html>\n"
         return web.Response(content_type="text/html", text=text)
 
+    async def html_charts(self, request):
+        """
+        Render apps.yaml as an HTML page
+        """
+        self.default_page = "./charts"
+        text = self.get_header("Predbat Config")
+        text += "<body>\n"
+        text += '<div id="chart"></div>'
+        text += self.get_chart()
+        text += "</body></html>\n"
+        return web.Response(content_type="text/html", text=text)
+
     async def html_apps(self, request):
         """
         Render apps.yaml as an HTML page
         """
+        self.default_page = "./apps"
         text = self.get_header("Predbat Config")
         text += "<body>\n"
         text += "<table>\n"
@@ -371,11 +492,12 @@ class WebInterface:
         Return the Predbat config as an HTML page
         """
 
+        self.default_page = "./config"
         text = self.get_header("Predbat Config", refresh=60)
         text += "<body>\n"
         text += '<form class="form-inline" action="./config" method="post" enctype="multipart/form-data" id="configform">\n'
         text += "<table>\n"
-        text += "<tr><th>Name</th><th>Entity</th><th>Type</th><th>Current</th><th>Default</th><th>Select</th></tr>\n"
+        text += "<tr><th></th><th>Name</th><th>Entity</th><th>Type</th><th>Current</th><th>Default</th><th>Select</th></tr>\n"
 
         input_number = """
             <input type="number" id="{}" name="{}" value="{}" min="{}" max="{}" step="{}" onchange="javascript: this.form.submit();">
@@ -393,15 +515,17 @@ class WebInterface:
                 itemtype = item.get("type", "")
                 default = item.get("default", "")
                 useid = entity.replace(".", "__")
+                unit = item.get("unit", "")
+                icon = self.icon2html(item.get("icon", ""))
 
                 if itemtype == "input_number" and item.get("step", 1) == 1:
                     value = int(value)
 
-                text += "<tr><td>{}</td><td>{}</td><td>{}</td>".format(friendly, entity, itemtype)
+                text += "<tr><td>{}</td><td>{}</td><td>{}</td><td>{}</td>".format(icon, friendly, entity, itemtype)
                 if value == default:
-                    text += '<td class="default">{}</td><td>{}</td>\n'.format(value, default)
+                    text += '<td class="default">{} {}</td><td>{} {}</td>\n'.format(value, unit, default, unit)
                 else:
-                    text += '<td class="modified">{}</td><td>{}</td>\n'.format(value, default)
+                    text += '<td class="modified">{} {}</td><td>{} {}</td>\n'.format(value, unit, default, unit)
 
                 if itemtype == "switch":
                     text += '<td><select name="{}" id="{}" onchange="javascript: this.form.submit();">'.format(useid, useid)
@@ -440,6 +564,7 @@ class WebInterface:
         text += '<td><h2>Predbat</h2></td><td><img src="https://github-production-user-asset-6210df.s3.amazonaws.com/48591903/249456079-e98a0720-d2cf-4b71-94ab-97fe09b3cee1.png" width="50" height="50"></td>\n'
         text += '<td><a href="./dash" target="main_frame">Dash</a></td>\n'
         text += '<td><a href="./plan" target="main_frame">Plan</a></td>\n'
+        text += '<td><a href="./charts" target="main_frame">Charts</a></td>\n'
         text += '<td><a href="./config" target="main_frame">Config</a></td>\n'
         text += '<td><a href="./apps" target="main_frame">apps.yaml</a></td>\n'
         text += '<td><a href="./log?warnings" target="main_frame">Log</a></td>\n'
@@ -455,7 +580,7 @@ class WebInterface:
         text += "<body>\n"
         text += '<div class="iframe-container">\n'
         text += '<iframe src="./menu" title="Menu frame" class="menu-frame" name="menu_frame"></iframe>\n'
-        text += '<iframe src="./dash" title="Main frame" class="main-frame" name="main_frame"></iframe>\n'
+        text += '<iframe src="{}" title="Main frame" class="main-frame" name="main_frame"></iframe>\n'.format(self.default_page)
         text += "</div>\n"
         text += "</body></html>\n"
         return web.Response(content_type="text/html", text=text)
