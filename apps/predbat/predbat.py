737197f1cf158758ad8e5f6862b77fcf1a71c117
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2024-09-22 21:48:16 +0100

    Fixes for timed discharge/charge
    https://github.com/springfall2008/batpred/issues/1440

--- apps/predbat/predbat.py
+++ apps/predbat/predbat.py
@@ -671,7 +671,7 @@ class PredBat(hass.Hass):
 
         if self.debug_enable:
             self.log("Download {}".format(url))
-
+        
         try:
             r = requests.get(url)
         except:
@@ -6745,7 +6745,6 @@ class PredBat(hass.Hass):
                 min_region_size = int(2 * 60)
                 while region_size >= min_region_size:
                     self.log(">> Region optimisation pass width {}".format(region_size))
-                    hit_end = False
                     for region in range(0, self.end_record + self.minutes_now, min_region_size):
                         region_end = min(region + region_size, self.end_record + self.minutes_now)
 
@@ -6796,7 +6795,7 @@ class PredBat(hass.Hass):
                             break
                     region_size = int(region_size / 2)
 
-            # Keep the freeze but not the full discharge as that will be re-introduced later
+            # Keep the freeze but not the full discharge as that will be re-introduced later        
             for window_n in range(len(ignore_discharge_limits)):
                 if ignore_discharge_limits[window_n] == 99.0:
                     self.discharge_limits_best[window_n] = 99.0
@@ -8383,7 +8382,7 @@ class PredBat(hass.Hass):
                 break
 
             resetDischarge = False
-            if (not self.set_discharge_during_charge) or (not self.car_charging_from_battery) or self.iboost_prevent_discharge:
+            if (not self.set_discharge_during_charge) or (not self.car_charging_from_battery) or self.iboost_prevent_discharge or inverter.inv_charge_discharge_with_rate:
                 # These options mess with discharge rate, so we must reset it when they aren't changing it
                 resetDischarge = True
 
@@ -11128,7 +11127,7 @@ class PredBat(hass.Hass):
         else:
             self.log("Info: Refresh config entities as config_refresh state is unknown")
             self.update_pending = True
-
+        
         # Database tick
         self.ha_interface.db_tick()
 
