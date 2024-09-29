c3e948023c951a7633a15d0859e3d2bac5b1b13a
iainfogg <iain@feedthemouse.com>
2024-09-29 14:18:31 +0000

    Another merge issue

--- apps/predbat/web.py
+++ apps/predbat/web.py
@@ -208,15 +208,24 @@ class WebInterface:
 <script>
 window.onresize = function(){ location.reload(); };
 var width = window.innerWidth;
+var height = window.innerHeight;
 if (width < 600) {
-    width = '600px';
-} else {
-    width = '100%';
+    width = 600
+}
+width = width - 50;
+height = height - 50;
+
+if (height * 1.68 > width) {
+   height = width / 1.68;
+}
+else {
+   width = height * 1.68;
 }
 var options = {
   chart: {
     type: 'line',
-    width: `${width}`
+    width: width,
+    height: height
   },
   span: {
     start: 'minute', offset: '-12h'
