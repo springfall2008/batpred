703337f0f5310cb2673adb9ed30dad2cf54544a5
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2024-10-13 17:06:05 +0100

    Tidy up log

--- apps/predbat/predheat.py
+++ apps/predbat/predheat.py
@@ -293,7 +293,7 @@ class PredHeat:
             if self.smart_thermostat:
                 if next_adjust and minute >= adjust["start"] and minute < adjust["end"]:
                     target_temp = adjust["to"]
-                    self.log("Predheat: Adjusted target temperature for smart heating to {} at minute {}".format(target_temp, minute))
+                    #self.log("Predheat: Adjusted target temperature for smart heating to {} at minute {}".format(target_temp, minute))
 
             temp_diff_outside = internal_temp - external_temp
             temp_diff_inside = target_temp - internal_temp
@@ -499,11 +499,7 @@ class PredHeat:
 
         self.heating_active = self.get_arg("heating_active", False, domain="predheat")
 
-        self.log(
-            "Predheat: Mode {} Heating active {} Heat loss watts {} degrees {} watts per degree {} heating energy so far {}".format(
-                self.mode, self.heating_active, self.heat_loss_watts, self.heat_loss_degrees, self.watt_per_degree, self.heat_energy_today
-            )
-        )
+        self.log("Predheat: Mode {} Heating active {} Heat loss watts {} degrees {} watts per degree {} heating energy so far {}".format(self.mode, self.heating_active, self.heat_loss_watts, self.heat_loss_degrees, self.watt_per_degree, self.dp2(self.heat_energy_today)))
         self.get_weather_data(now_utc)
         status = "idle"
 
