0a932f420f95bc4c8fc36363153de2912d3502e6
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2024-12-14 15:51:56 +0000

    Bug with metric_min_improvement_discharge being changed to integer (#1733)
    * Bug with metric_min_improvement_discharge being changed to integer
    
    https://github.com/springfall2008/batpred/issues/1731
    
    * [pre-commit.ci lite] apply automatic fixes
    
    * Merge
    
    ---------
    
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- apps/predbat/config.py
+++ apps/predbat/config.py
@@ -321,7 +321,7 @@ CONFIG_ITEMS = [
         "unit": "p",
         "icon": "mdi:currency-usd",
         "enable": "expert_mode",
-        "default": 5,
+        "default": 5.0,
     },
     {
         "name": "metric_battery_cycle",
