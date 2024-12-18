61540bcb49f1077db813f9f5beba2e4a7de45bbe
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2024-12-18 22:35:35 +0000

    Weather comp fixes

--- apps/predbat/predheat.py
+++ apps/predbat/predheat.py
@@ -135,13 +135,13 @@ class PredHeat:
         if weather_compensation:
             for key, value in weather_compensation.items():
                 self.weather_compensation[key] = value
+            self.weather_compensation = self.fill_table_gaps(self.weather_compensation)
             self.weather_compensation_enabled = True
 
         # Fill gaps in tables
         self.delta_correction = self.fill_table_gaps(self.delta_correction)
         self.gas_efficiency = self.fill_table_gaps(self.gas_efficiency)
         self.heat_pump_efficiency = self.fill_table_gaps(self.heat_pump_efficiency)
-        self.weather_compensation = self.fill_table_gaps(self.weather_compensation)
 
         self.heat_pump_efficiency_max = max(self.heat_pump_efficiency.values())
         self.log("Predheat: Delta correction {}".format(self.delta_correction))
