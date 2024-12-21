7fc19cae3d0e9a568c1a6dbd0ebd0a49af248b77
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2024-12-21 13:11:55 +0000

    Ignore planned IO slots when car is unplugged and option is set
    https://github.com/springfall2008/batpred/issues/1762

--- apps/predbat/fetch.py
+++ apps/predbat/fetch.py
@@ -886,7 +886,8 @@ class Fetch:
             # Completed and planned slots
             if completed:
                 self.octopus_slots += completed
-            if planned:
+            if planned and (not self.octopus_intelligent_ignore_unplugged or self.car_charging_planned[0]):
+                # We only count planned slots if the car is plugged in or we are ignoring unplugged cars
                 self.octopus_slots += planned
 
             # Get rate for import to compute charging costs
