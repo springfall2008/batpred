dfc1f0b15ddd602b38e534a01a524f42e5dd57ca
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2025-01-26 16:06:43 +0000

    Energy data service cleanup and unit tests

--- apps/predbat/unit_test.py
+++ apps/predbat/unit_test.py
@@ -74,16 +74,16 @@ class TestHAInterface:
             result = self.dummy_items[entity_id]
             if isinstance(result, dict):
                 if attribute:
-                    result = result.get("attributes", {}).get(attribute, default)
+                    result = result.get(attribute, default)
                 else:
                     result = result.get("state", default)
             else:
                 if attribute:
                     result = default
-            # print("Getting state: {} attribute {} => {}".format(entity_id, attribute, result))
+            print("Getting state: {} attribute {} => {}".format(entity_id, attribute, result))
             return result
         else:
-            # print("Getting state: {} attribute {} => default".format(entity_id, default))
+            print("Getting state: {} attribute {} => default".format(entity_id, default))
             return default
 
     def call_service(self, service, **kwargs):
@@ -6629,6 +6629,165 @@ def test_find_charge_rate(my_predbat):
         failed = 1
     return failed
 
+def test_energydataservice(my_predbat):
+    """
+    Test the energy data service
+    """
+    failed = 0
+
+    print("Test energy data service")
+
+    date_yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+    today = datetime.now().strftime("%Y-%m-%d")
+    tomorrow = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d")
+
+    data_example = f"""
+state: 1.34
+state_class: total
+current_price: 1.2706032
+unit: kWh
+currency: EUR
+region: Finland
+region_code: FI
+tomorrow_valid: true
+next_data_update: 13:39:54
+today: 1.242, 1.242, 1.242, 1.242, 1.243, 1.243, 1.243, 1.243, 1.243, 1.243, 1.244, 1.245, 1.246, 1.246, 1.261, 1.271, 1.286, 1.295, 1.295, 1.288, 1.296, 1.284, 1.273, 1.289
+tomorrow: 1.284, 1.273, 1.263, 1.283, 1.309, 1.333, 1.385, 1.389, 1.366, 1.336, 1.323, 1.316, 1.318, 1.338, 1.338, 1.314, 1.299, 1.299, 1.297, 1.286, 1.281, 1.277, 1.268
+raw_today: 
+    - hour: '{today}T00:00:00+00:00'
+      price: 1.242
+    - hour: '{today}T01:00:00+00:00'
+      price: 1.242
+    - hour: '{today}T02:00:00+00:00'
+      price: 1.242
+    - hour: '{today}T03:00:00+00:00'
+      price: 1.242
+    - hour: '{today}T04:00:00+00:00'
+      price: 1.243
+    - hour: '{today}T05:00:00+00:00'
+      price: 1.243
+    - hour: '{today}T06:00:00+00:00'
+      price: 1.243
+    - hour: '{today}T07:00:00+00:00'
+      price: 1.243
+    - hour: '{today}T08:00:00+00:00'
+      price: 1.243
+    - hour: '{today}T09:00:00+00:00'
+      price: 1.243
+    - hour: '{today}T10:00:00+00:00'
+      price: 1.244
+    - hour: '{today}T11:00:00+00:00'
+      price: 1.245
+    - hour: '{today}T12:00:00+00:00'
+      price: 1.246
+    - hour: '{today}T13:00:00+00:00'
+      price: 1.246
+    - hour: '{today}T14:00:00+00:00'
+      price: 1.261
+    - hour: '{today}T15:00:00+00:00'
+      price: 1.271
+    - hour: '{today}T16:00:00+00:00'
+      price: 1.286
+    - hour: '{today}T17:00:00+00:00'
+      price: 1.295
+    - hour: '{today}T18:00:00+00:00'
+      price: 1.295
+    - hour: '{today}T19:00:00+00:00'
+      price: 1.288
+    - hour: '{today}T20:00:00+00:00'
+      price: 1.296
+    - hour: '{today}T21:00:00+00:00'
+      price: 1.284
+    - hour: '{today}T22:00:00+00:00'
+      price: 1.273
+    - hour: '{today}T23:00:00+00:00'
+      price: 1.289
+raw_tomorrow: 
+    - hour: '{tomorrow}T00:00:00+00:00'
+      price: 1.284
+    - hour: '{tomorrow}T01:00:00+00:00'
+      price: 1.273
+    - hour: '{tomorrow}T02:00:00+00:00'
+      price: 1.263
+    - hour: '{tomorrow}T03:00:00+00:00'
+      price: 1.283
+    - hour: '{tomorrow}T04:00:00+00:00'
+      price: 1.309
+    - hour: '{tomorrow}T05:00:00+00:00'
+      price: 1.333
+    - hour: '{tomorrow}T06:00:00+00:00'
+      price: 1.385
+    - hour: '{tomorrow}T07:00:00+00:00'
+      price: 1.389
+    - hour: '{tomorrow}T08:00:00+00:00'
+      price: 1.366
+    - hour: '{tomorrow}T09:00:00+00:00'
+      price: 1.336
+    - hour: '{tomorrow}T10:00:00+00:00'
+      price: 1.323
+    - hour: '{tomorrow}T11:00:00+00:00'
+      price: 1.316
+    - hour: '{tomorrow}T12:00:00+00:00'
+      price: 1.318
+    - hour: '{tomorrow}T13:00:00+00:00'
+      price: 1.338
+    - hour: '{tomorrow}T14:00:00+00:00'
+      price: 1.338
+    - hour: '{tomorrow}T15:00:00+00:00'
+      price: 1.314
+    - hour: '{tomorrow}T16:00:00+00:00'
+      price: 1.299
+    - hour: '{tomorrow}T17:00:00+00:00'
+      price: 1.299
+    - hour: '{tomorrow}T18:00:00+00:00'
+      price: 1.297
+    - hour: '{tomorrow}T19:00:00+00:00'
+      price: 1.286
+    - hour: '{tomorrow}T20:00:00+00:00'
+      price: 1.281
+    - hour: '{tomorrow}T21:00:00+00:00'
+      price: 1.277
+    - hour: '{tomorrow}T22:00:00+00:00'
+      price: 1.268
+    - hour: '{tomorrow}T23:00:00+00:00'
+      price: 1.268
+today_min: 
+    hour: '{today}T00:00:00+00:00'
+    price: 1.242
+today_max: 
+    hour: '{today}T20:00:00+00:00'
+    price: 1.296
+today_mean: 1.26
+tomorrow_min: 
+    hour: '{tomorrow}T02:00:00+00:00'
+    price: 1.263
+tomorrow_max: 
+    hour: '{tomorrow}T07:00:00+00:00'
+    price: 1.389
+tomorrow_mean: 1.312
+use_cent: false
+attribution: Data sourced from Nord Pool
+unit_of_measurement: EUR/kWh
+device_class: monetary
+icon: mdi:flash
+friendly_name: Energi Data Service
+"""
+
+    ha = my_predbat.ha_interface
+    ha.dummy_items["sensor.energi_data_service"] = yaml.safe_load(data_example)
+    my_predbat.args["energi_data_service"] = "sensor.energi_data_service"
+    rates = my_predbat.fetch_energidataservice_rates("sensor.energi_data_service")
+
+    show = []
+    for minute in range(0, 48*60, 15):
+        show.append(rates[minute])
+
+    expected_show = [0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0124, 0.0125, 0.0125, 0.0125, 0.0125, 0.0125, 0.0125, 0.0125, 0.0125, 0.0125, 0.0125, 0.0125, 0.0125, 0.0126, 0.0126, 0.0126, 0.0126, 0.0127, 0.0127, 0.0127, 0.0127, 0.0129, 0.0129, 0.0129, 0.0129, 0.0129, 0.0129, 0.0129, 0.0129, 0.0129, 0.0129, 0.0129, 0.0129, 0.0129, 0.0129, 0.0129, 0.0129, 0.013, 0.013, 0.013, 0.013, 0.0128, 0.0128, 0.0128, 0.0128, 0.0127, 0.0127, 0.0127, 0.0127, 0.0129, 0.0129, 0.0129, 0.0129, 0.0128, 0.0128, 0.0128, 0.0128, 0.0127, 0.0127, 0.0127, 0.0127, 0.0126, 0.0126, 0.0126, 0.0126, 0.0128, 0.0128, 0.0128, 0.0128, 0.0131, 0.0131, 0.0131, 0.0131, 0.0133, 0.0133, 0.0133, 0.0133, 0.0138, 0.0138, 0.0138, 0.0138, 0.0139, 0.0139, 0.0139, 0.0139, 0.0137, 0.0137, 0.0137, 0.0137, 0.0134, 0.0134, 0.0134, 0.0134, 0.0132, 0.0132, 0.0132, 0.0132, 0.0132, 0.0132, 0.0132, 0.0132, 0.0132, 0.0132, 0.0132, 0.0132, 0.0134, 0.0134, 0.0134, 0.0134, 0.0134, 0.0134, 0.0134, 0.0134, 0.0131, 0.0131, 0.0131, 0.0131, 0.013, 0.013, 0.013, 0.013, 0.013, 0.013, 0.013, 0.013, 0.013, 0.013, 0.013, 0.013, 0.0129, 0.0129, 0.0129, 0.0129, 0.0128, 0.0128, 0.0128, 0.0128, 0.0128, 0.0128, 0.0128, 0.0128, 0.0127, 0.0127, 0.0127, 0.0127, 0.0127, 0.0127, 0.0127, 0.0127]
+    if json.dumps(show) != json.dumps(expected_show):
+        print("ERROR: Expecting show should be {} got {}".format(expected_show, show))
+        failed = 1    
+
+    return failed
 
 def test_saving_session(my_predbat):
     """
@@ -6656,37 +6815,36 @@ friendly_name: Octoplus Saving Session (A-4DD6C5EE)
 
     session_sensor = f"""
 state: '2025-01-23T12:10:11.108+00:00'
-attributes:
-    event_types: octopus_energy_all_octoplus_saving_sessions
-    event_type: octopus_energy_all_octoplus_saving_sessions
-    account_id: A-4DD6C5EE
-    available_events:
-      - id: 1336
-        start: '{date_today}T18:30:00+00:00'
-        end: '{date_today}T19:30:00+00:00'
-        duration_in_minutes: 60
-        rewarded_octopoints: null
-        octopoints_per_kwh: 500
-        code: 987654
-    joined_events:
-      - id: 1327
-        start: '{date_last_year}T17:00:00+00:00'
-        end: '{date_last_year}T18:00:00+00:00'
-        duration_in_minutes: 60
-        rewarded_octopoints: 936
-        octopoints_per_kwh: 576
-      - id: 1334
-        start: '{date_yesterday}T17:30:00+00:00'
-        end: '{date_yesterday}T18:30:00+00:00'
-        duration_in_minutes: 60
-        rewarded_octopoints: null
-        octopoints_per_kwh: 192
-      - id: 1335
-        start: '{date_today}T16:30:00+00:00'
-        end: '{date_today}T17:30:00+00:00'
-        duration_in_minutes: 60
-        rewarded_octopoints: null
-        octopoints_per_kwh: 448
+event_types: octopus_energy_all_octoplus_saving_sessions
+event_type: octopus_energy_all_octoplus_saving_sessions
+account_id: A-4DD6C5EE
+available_events:
+    - id: 1336
+      start: '{date_today}T18:30:00+00:00'
+      end: '{date_today}T19:30:00+00:00'
+      duration_in_minutes: 60
+      rewarded_octopoints: null
+      octopoints_per_kwh: 500
+      code: 987654
+joined_events:
+    - id: 1327
+      start: '{date_last_year}T17:00:00+00:00'
+      end: '{date_last_year}T18:00:00+00:00'
+      duration_in_minutes: 60
+      rewarded_octopoints: 936
+      octopoints_per_kwh: 576
+    - id: 1334
+      start: '{date_yesterday}T17:30:00+00:00'
+      end: '{date_yesterday}T18:30:00+00:00'
+      duration_in_minutes: 60
+      rewarded_octopoints: null
+      octopoints_per_kwh: 192
+    - id: 1335
+      start: '{date_today}T16:30:00+00:00'
+      end: '{date_today}T17:30:00+00:00'
+      duration_in_minutes: 60
+      rewarded_octopoints: null
+      octopoints_per_kwh: 448
 friendly_name: Octoplus Saving Session Events (A-12345678)
 """.format(
         date_last_year=date_last_year, date_yesterday=date_yesterday, date_today=date_today
@@ -6759,6 +6917,8 @@ def main():
 
     if not failed:
         failed |= test_find_charge_rate(my_predbat)
+    if not failed:
+        failed |= test_energydataservice(my_predbat)
     if not failed:
         failed |= test_saving_session(my_predbat)
     free_sessions = my_predbat.download_octopus_free("http://octopus.energy/free-electricity")
