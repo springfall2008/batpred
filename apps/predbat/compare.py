01c240f627baf9af570cbcf0046726a3e8ef779d
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2025-02-08 16:53:58 +0000

    Tariff compare to re-plan car

--- apps/predbat/compare.py
+++ apps/predbat/compare.py
@@ -16,7 +16,6 @@ import copy
 # 2. Allow change to start/end comparison points e.g. tomorrow or today
 # 3. Consider Octopus API key to access current tariff info and switch links
 
-
 class Compare:
     def __init__(self, my_predbat):
         self.pb = my_predbat
@@ -42,6 +41,7 @@ class Compare:
         self.pb.fetch_config_options()
 
     def fetch_rates(self, tariff, rate_import_base, rate_export_base):
+
         pb = self.pb
 
         # Reset threshold to automatic
@@ -63,16 +63,16 @@ class Compare:
             if entity_id:
                 pb.rate_import = pb.fetch_octopus_rates(entity_id, adjust_key="is_intelligent_adjusted")
             else:
-                self.log("Warn: Compare tariff {} bad Octopus entity id {}".format(tariff.get("id", ""), entity_id))
+                self.log("Warn: Compare tariff {} bad Octopus entity id {}".format(tariff.get('id', ''), entity_id))
         elif "metric_energidataservice_import" in tariff:
             # Octopus import rates
             entity_id = pb.resolve_arg("metric_energidataservice_import", tariff["metric_energidataservice_import"])
             if entity_id:
                 pb.rate_import = pb.fetch_energidataservice_rates(entity_id, adjust_key="is_intelligent_adjusted")
             else:
-                self.log("Warn: Compare tariff {} bad Energidata entity id {}".format(tariff.get("id", ""), entity_id))
-        elif "rates_import" in tariff:
-            pb.rate_import = pb.basic_rates(tariff["rates_import"], "rates_import")
+                self.log("Warn: Compare tariff {} bad Energidata entity id {}".format(tariff.get('id', ''), entity_id))
+        elif 'rates_import' in tariff:
+            pb.rate_import = pb.basic_rates(tariff['rates_import'], "rates_import")
         else:
             self.log("Using existing rate import data")
 
@@ -83,25 +83,25 @@ class Compare:
             # Octopus export rates
             entity_id = pb.resolve_arg("metric_octopus_export", tariff["metric_octopus_export"])
             if entity_id:
-                pb.rate_export = pb.fetch_octopus_rates(entity_id)
+                pb.rate_export= pb.fetch_octopus_rates(entity_id)
             else:
-                self.log("Warn: Compare tariff {} bad Octopus entity id {}".format(tariff.get("id", ""), entity_id))
+                self.log("Warn: Compare tariff {} bad Octopus entity id {}".format(tariff.get('id', ''), entity_id))
         elif "metric_energidataservice_export" in tariff:
             # Octopus import rates
             entity_id = pb.resolve_arg("metric_energidataservice_export", tariff["metric_energidataservice_export"])
             if entity_id:
                 pb.rate_export = pb.fetch_energidataservice_rates(entity_id, adjust_key="is_intelligent_adjusted")
             else:
-                self.log("Warn: Compare tariff {} bad Energidata entity id {}".format(tariff.get("id", ""), entity_id))
-        elif "rates_export" in tariff:
-            pb.rate_export = pb.basic_rates(tariff["rates_export"], "rates_export")
+                self.log("Warn: Compare tariff {} bad Energidata entity id {}".format(tariff.get('id', ''), entity_id))
+        elif 'rates_export' in tariff:
+            pb.rate_export = pb.basic_rates(tariff['rates_export'], "rates_export")
         else:
             self.log("Using existing rate export data")
 
         if pb.rate_import:
             pb.rate_scan(pb.rate_import, print=False)
             pb.rate_import, pb.rate_import_replicated = pb.rate_replicate(pb.rate_import, pb.io_adjusted, is_import=True)
-            if "rates_import_override" in tariff:
+            if 'rates_import_override' in tariff:
                 pb.rate_import = pb.basic_rates(tariff["rates_import_override"], "rates_import_override", pb.rate_import, pb.rate_import_replicated)
             pb.rate_scan(pb.rate_import, print=True)
 
@@ -109,7 +109,7 @@ class Compare:
         if pb.rate_export:
             pb.rate_scan_export(pb.rate_export, print=False)
             pb.rate_export, pb.rate_export_replicated = pb.rate_replicate(pb.rate_export, is_import=False)
-            if "rates_export_override" in tariff:
+            if 'rates_export_override' in tariff:
                 pb.rate_export = pb.basic_rates(tariff["rates_export_override"], "rates_export_override", pb.rate_export, pb.rate_export_replicated)
             pb.rate_scan_export(pb.rate_export, print=True)
 
@@ -130,7 +130,7 @@ class Compare:
             pb.low_rates, lowest, highest = pb.rate_scan_window(pb.rate_import, 5, pb.rate_import_cost_threshold, False)
             # Update threshold automatically
             if pb.rate_low_threshold == 0 and highest >= pb.rate_min:
-                pb.rate_import_cost_threshold = highest
+                pb.rate_import_cost_threshold = highest    
 
         # Compare to see if rates changes
         for minute in range(pb.minutes_now, pb.forecast_minutes + pb.minutes_now):
@@ -153,15 +153,22 @@ class Compare:
         my_predbat.calculate_plan(recompute=True, debug_mode=False, publish=False)
 
         cost, import_kwh_battery, import_kwh_house, export_kwh, soc_min, soc, soc_min_minute, battery_cycle, metric_keep, final_iboost, final_carbon_g = my_predbat.run_prediction(
-            my_predbat.charge_limit_best, my_predbat.charge_window_best, my_predbat.export_window_best, my_predbat.export_limits_best, False, end_record=end_record, save="compare"
+           my_predbat.charge_limit_best, 
+           my_predbat.charge_window_best, 
+           my_predbat.export_window_best, 
+           my_predbat.export_limits_best, 
+           False, 
+           end_record=end_record, 
+           save="compare"
         )
         cost10, import_kwh_battery10, import_kwh_house10, export_kwh10, soc_min10, soc10, soc_min_minute10, battery_cycle10, metric_keep10, final_iboost10, final_carbon_g10 = my_predbat.run_prediction(
-            my_predbat.charge_limit_best,
-            my_predbat.charge_window_best,
-            my_predbat.export_window_best,
-            my_predbat.export_limits_best,
-            True,
-            end_record=end_record,
+           my_predbat.charge_limit_best, 
+           my_predbat.charge_window_best, 
+           my_predbat.export_window_best, 
+           my_predbat.export_limits_best, 
+           True, 
+           end_record=end_record, 
+
         )
         # Work out value of the battery at the start end end of the period to allow for the change in SOC
         metric_start, battery_value_start = my_predbat.compute_metric(end_record, my_predbat.soc_kw, my_predbat.soc_kw, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
@@ -204,7 +211,8 @@ class Compare:
 
         return result_data
 
-    def run_single(self, tariff, rate_import_base, rate_export_base, end_record, debug=False, fetch_sensor=True):
+    def run_single(self, tariff, rate_import_base, rate_export_base, end_record, debug=False, fetch_sensor=True, car_charging_slots=[]):
+
         """
         Compare a single energy tariff with the current settings and report results
         """
@@ -234,7 +242,7 @@ class Compare:
         my_predbat.import_today_now = 0
         my_predbat.export_today_now = 0
 
-        # Change to a fixed 48 hour plan
+        # Change to a fixed 48 hour plan
         my_predbat.forecast_plan_hours = 48
         my_predbat.forecast_minutes = my_predbat.forecast_plan_hours * 60
         my_predbat.forecast_days = my_predbat.forecast_plan_hours / 24
@@ -246,15 +254,19 @@ class Compare:
         my_predbat.manual_freeze_export_times = []
         my_predbat.manual_demand_times = []
         my_predbat.manual_all_times = []
+        my_predbat.octopus_intelligent_charging = False
+
+        self.recompute_car_charging(car_charging_slots)
 
         self.log("Running scenario for tariff: {}".format(name))
         result_data = self.run_scenario(end_record)
-        result_data["existing_tariff"] = existing_tariff
+        result_data['existing_tariff'] = existing_tariff
         self.log("Scenario complete for tariff: {} cost {} metric {}".format(name, result_data["cost"], result_data["metric"]))
         if debug:
             with open("compare_{}.html".format(tariff_id), "w") as f:
-                f.write(result_data["html"])
+                f.write(result_data['html'])
         return result_data
+
 
     def select_best(self, compare_list, results):
         """
@@ -293,9 +305,9 @@ class Compare:
                 try:
                     data = yaml.safe_load(f)
                     if data:
-                        self.comparisons = data.get("comparisons", {})
+                        self.comparisons = data.get("comparisons", {})                        
                 except yaml.YAMLError as exc:
-                    self.log("Error loading comparisons: {}".format(exc))
+                    self.log("Error loading comparisons: {}".format(exc))                    
 
         if self.comparisons:
             compare_list = self.pb.get_arg("compare_list", [])
@@ -323,7 +335,7 @@ class Compare:
             if result:
                 cost = result.get("cost", 0)
                 name = result.get("name", "")
-                attributes = {
+                attributes={
                     "friendly_name": "Compare " + name,
                     "state_class": "measurement",
                     "unit_of_measurement": "p",
@@ -332,14 +344,14 @@ class Compare:
                 for item in result:
                     value = result[item]
                     if item != "html":
-                        attributes[item] = value
+                        attributes[item] = value                    
 
                 entity_id = self.prefix + ".compare_tariff_" + tariff_id
                 self.dashboard_item(
                     entity_id,
                     state=cost,
                     attributes=attributes,
-                )
+                )  
                 result["entity_id"] = entity_id
 
     def publish_only(self):
@@ -353,11 +365,40 @@ class Compare:
         self.select_best(compare_list, self.comparisons)
         self.publish_data()
 
+    def recompute_car_charging(self, car_charging_slots):
+        """
+        Recompute car charging plan
+        """
+        my_predbat = self.pb
+
+        my_predbat.car_charging_slots = [[] for car_n in range(my_predbat.num_cars)]
+
+        for car_n in range(my_predbat.num_cars):
+            total_car_kwh = 0
+            if len(car_charging_slots) > car_n and car_charging_slots[car_n]:
+                for car_slot in car_charging_slots[car_n]:
+                    total_car_kwh += car_slot.get("kwh", 0)
+            if total_car_kwh > 0:
+                my_predbat.car_charging_soc[car_n] = 0
+                my_predbat.car_charging_limit[car_n] = total_car_kwh
+                my_predbat.car_charging_battery_size[car_n] = 100
+            else:
+                my_predbat.car_charging_soc[car_n] = my_predbat.car_charging_battery_size[car_n]
+                my_predbat.car_charging_limit[car_n] = my_predbat.car_charging_battery_size[car_n]
+
+            if my_predbat.car_charging_planned[car_n] or my_predbat.car_charging_now[car_n]:
+                self.log("Re-plan car {} for charing to {}".format(car_n, my_predbat.car_charging_limit[car_n]))
+                my_predbat.car_charging_plan_smart[car_n] = True
+                my_predbat.car_charging_slots[car_n] = my_predbat.plan_car_charging(car_n, my_predbat.low_rates)
+
+            if my_predbat.car_charging_planned[car_n] and my_predbat.car_charging_exclusive[car_n]:                    
+                break
+
     def run_all(self, debug=False, fetch_sensor=True):
         """
         Compare a comparison in prices across multiple energy tariffs and report results
         take care not to destroy the state of the system for the primary settings
-        """
+        """        
         compare_list = self.pb.get_arg("compare_list", [])
         if not compare_list:
             return
@@ -385,6 +426,12 @@ class Compare:
         save_iboost_today = my_predbat.iboost_today
         save_import_today_now = my_predbat.import_today_now
         save_export_today_now = my_predbat.export_today_now
+        save_octopus_intelligent_charging = my_predbat.octopus_intelligent_charging
+        save_car_charging_plan_smart = copy.deepcopy(my_predbat.car_charging_plan_smart)
+        save_car_charging_limit = copy.deepcopy(my_predbat.car_charging_limit)
+        save_car_charging_soc = copy.deepcopy(my_predbat.car_charging_soc)
+        save_car_charging_battery_size = copy.deepcopy(my_predbat.car_charging_battery_size)
+        save_car_charging_slots = copy.deepcopy(my_predbat.car_charging_slots)
 
         # Final reports, cut end_record back to 24 hours to ignore the dump at end of day
         end_record = int((my_predbat.minutes_now + 24 * 60 + 29) / 30) * 30 - my_predbat.minutes_now
@@ -396,7 +443,7 @@ class Compare:
         self.log("Starting comparison of tariffs")
 
         for tariff in compare_list:
-            result_data = self.run_single(tariff, rate_import_base, rate_export_base, end_record, debug=debug, fetch_sensor=fetch_sensor)
+            result_data = self.run_single(tariff, rate_import_base, rate_export_base, end_record, debug=debug, fetch_sensor=fetch_sensor, car_charging_slots=save_car_charging_slots)
             results[tariff["id"]] = result_data
             # Save and update comparisons as we go so it is updated in HA
             self.select_best(compare_list, results)
@@ -424,3 +471,10 @@ class Compare:
         my_predbat.iboost_today = save_iboost_today
         my_predbat.import_today_now = save_import_today_now
         my_predbat.export_today_now = save_export_today_now
+        my_predbat.octopus_intelligent_charging = save_octopus_intelligent_charging
+        my_predbat.car_charging_limit = save_car_charging_limit
+        my_predbat.car_charging_soc = save_car_charging_soc
+        my_predbat.car_charging_battery_size = save_car_charging_battery_size
+        my_predbat.car_charging_slots = save_car_charging_slots
+        my_predbat.car_charging_plan_smart = save_car_charging_plan_smart
+        
