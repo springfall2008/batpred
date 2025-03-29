1156548af443e911412d41db6ea5395f8fbb25d6
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2025-03-29 09:57:07 +0000

    Fix problem with intelligent dispatch sensor updates being mis-timed (#2152)
    * Fix problem with intelligent dispatch sensor updates being mis-timed
    
    * Updated

--- apps/predbat/octopus.py
+++ apps/predbat/octopus.py
@@ -322,28 +322,31 @@ class OctopusAPI:
         """
         Main run loop
         """
-        count_seconds = 0
-
         # Load cached data
         await self.load_octopus_cache()
 
+        first = True
         while not self.stop_api:
-            # 30 minute update
             try:
-                if count_seconds % (30 * 60) == 0:
-                    self.now = datetime.now()
-                    self.now_utc = datetime.now(timezone.utc).astimezone()
+                # Update time every minute
+                self.now = datetime.now()
+                self.now_utc = datetime.now(timezone.utc).astimezone()
+                count_minutes = self.now_utc.minute + self.now_utc.hour * 60
+
+                if first or (count_minutes % 30) == 0:
+                    # 30-minute update for tariff
                     self.midnight_utc = self.now_utc.replace(hour=0, minute=0, second=0, microsecond=0)
-                    token = await self.async_refresh_token()
                     self.account_data = await self.async_get_account(self.account_id)
                     self.tariffs = await self.async_find_tariffs()
 
-                if count_seconds % (10 * 60) == 0:
-                    self.intelligent_device = await self.async_update_intelligent_device(self.account_id)
+                if first or (count_minutes % 10) == 0:
+                    # 10-minute update for intelligent device
+                    await self.async_update_intelligent_device(self.account_id)
                     await self.fetch_tariffs(self.tariffs)
                     self.saving_sessions = await self.async_get_saving_sessions(self.account_id)
 
-                if count_seconds % (2 * 60) == 0:
+                if first or (count_minutes % 2) == 0:
+                    # 2-minute update for intelligent device sensor
                     await self.async_intelligent_update_sensor(self.account_id)
                     await self.save_octopus_cache()
 
@@ -352,11 +355,12 @@ class OctopusAPI:
                 if not self.api_started:
                     print("Octopus API: Started")
                     self.api_started = True
+                first = False
 
             except Exception as e:
                 self.log("Error: Octopus API: {}".format(e))
-            await asyncio.sleep(5)
-            count_seconds += 5
+
+            await asyncio.sleep(60)
         await self.api.async_close()
         print("Octopus API: Stopped")
 
@@ -465,11 +469,10 @@ class OctopusAPI:
         """
         import_tariff = self.tariffs.get("import", {})
         deviceID = import_tariff.get("deviceID", None)
-        completed_dispatches = self.get_intelligent_completed_dispatches()
-        device = {}
         if deviceID:
-            device = await self.async_get_intelligent_device(account_id, deviceID, completed_dispatches)
-        return device
+            completed_dispatches = self.get_intelligent_completed_dispatches()
+            self.intelligent_device = await self.async_get_intelligent_device(account_id, deviceID, completed_dispatches)
+        return self.intelligent_device
 
     def join_saving_session_event(self, event_code):
         """
