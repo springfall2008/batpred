2806e944a617851e31141a17996da10adc6b2826
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2025-03-31 21:43:52 +0100

    Fix to issue with new HA release and connection errors (#2181)
    * Fix to issue with new HA release and connection errors
    
    * Update ha.py

--- apps/predbat/ha.py
+++ apps/predbat/ha.py
@@ -85,7 +85,7 @@ class HAInterface:
             else:
                 self.log("Info: Using SQL Lite database as primary data source, no HA interface available")
         else:
-            check = self.api_call("/api/")
+            check = self.api_call("/api/services")
             if not check:
                 self.log("Warn: Unable to connect directly to Home Assistant at {}, please check your configuration of ha_url/ha_key".format(self.ha_url))
                 self.ha_key = None
