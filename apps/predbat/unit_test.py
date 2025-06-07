9c7bf5377214e86a5e16607e6c2a273db51f8ccf
Trefor Southwell <tdlj@tdlj.net>
2025-06-07 19:45:43 +0100

    Alt rate code

--- apps/predbat/unit_test.py
+++ apps/predbat/unit_test.py
@@ -3063,7 +3063,7 @@ def run_single_debug(test_name, my_predbat, debug_file, expected_file=None, comp
 
         # Find discharging windows
         if my_predbat.rate_export:
-            my_predbat.high_export_rates, export_lowest, export_highest = my_predbat.rate_scan_window(my_predbat.rate_export, 5, my_predbat.rate_export_cost_threshold, True)
+            my_predbat.high_export_rates, export_lowest, export_highest = my_predbat.rate_scan_window(my_predbat.rate_export, 5, my_predbat.rate_export_cost_threshold, True, alt_rates=my_predbat.rate_import)
             print("High export rate found rates in range {} to {} based on threshold {}".format(export_lowest, export_highest, my_predbat.rate_export_cost_threshold))
             print("Export windows {}".format(my_predbat.high_export_rates))
             # Update threshold automatically
@@ -3074,7 +3074,7 @@ def run_single_debug(test_name, my_predbat, debug_file, expected_file=None, comp
         if my_predbat.rate_import:
             # Find charging window
             print("rate scan window import threshold rate {}".format(my_predbat.rate_import_cost_threshold))
-            my_predbat.low_rates, lowest, highest = my_predbat.rate_scan_window(my_predbat.rate_import, 5, my_predbat.rate_import_cost_threshold, False)
+            my_predbat.low_rates, lowest, highest = my_predbat.rate_scan_window(my_predbat.rate_import, 5, my_predbat.rate_import_cost_threshold, False, alt_rates=my_predbat.rate_export)
             # Update threshold automatically
             if my_predbat.rate_low_threshold == 0 and highest >= my_predbat.rate_min:
                 my_predbat.rate_import_cost_threshold = highest
