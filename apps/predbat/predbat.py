8c7befa29222986eebd5747713b09aec9cb4a10f
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2025-06-22 12:36:36 +0100

    Fix set number unit conversion (#2496)

--- apps/predbat/predbat.py
+++ apps/predbat/predbat.py
@@ -36,7 +36,7 @@ import pytz
 import requests
 import asyncio
 
-THIS_VERSION = "v8.21.8"
+THIS_VERSION = "v8.21.9"
 
 # fmt: off
 PREDBAT_FILES = ["predbat.py", "config.py", "prediction.py", "gecloud.py","utils.py", "inverter.py", "ha.py", "download.py", "unit_test.py", "web.py", "predheat.py", "futurerate.py", "octopus.py", "solcast.py","execute.py", "plan.py", "fetch.py", "output.py", "userinterface.py", "energydataservice.py", "alertfeed.py", "compare.py", "db_manager.py", "db_engine.py"]
@@ -222,7 +222,8 @@ class PredBat(hass.Hass, Octopus, Energidataservice, Solcast, GECloud, Alertfeed
         if not required_unit:
             return state
 
-        units = self.ha_interface.get_state(entity_id=entity_id, default="", attribute="unit_of_measurement")
+        if not units:
+            units = self.ha_interface.get_state(entity_id=entity_id, default="", attribute="unit_of_measurement")
         if not units:
             return state
 
