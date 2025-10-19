f054cf899a8eef411ca9dfa3b2ed91de57b85e1f
Trefor Southwell <tdlj@tdlj.net>
2025-10-19 20:26:30 +0100

    Typo

--- apps/predbat/gecloud.py
+++ apps/predbat/gecloud.py
@@ -846,7 +846,7 @@ class GECloudDirect:
                         await self.publish_evc_data(serial, self.evc_data[uuid])
 
                 if seconds % (10 * 60) == 0:
-                    # Get All registers every now and again incase user changes them
+                    # Get All registers every now and again in case user changes them
                     for device in device_list:
                         if seconds == 0 or self.polling_mode or (device == ems_device) or (device == gateway_device):
                             self.settings[device] = await self.async_get_inverter_settings(device, first=False, previous=self.settings.get(device, {}))
