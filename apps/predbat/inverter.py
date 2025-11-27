8b9a6cccb27b0c08ed9f0468fe389bba13655393
Tom Carpenter <T.Carpenter@leeds.ac.uk>
2025-11-27 00:22:46 +0000

    Allow charge curves to detect multi-% jumps
    With some batteries it is possible for the final charge curve to jump more than one percent within any given minute, particularly towards the end of the curve.
    
    Allow the code to correctly handle this by averaging the power across the total number of percentage points crossed in a given step. For example if SoC charged 2% in 1 minute, then the average charger each of the two %age points will actually be half the calculated amount.

--- apps/predbat/inverter.py
+++ apps/predbat/inverter.py
@@ -641,9 +641,10 @@ class Inverter:
                 for data_point in search_range:
                     for minute in range(1, min_len):
                         # Start trigger is when the SOC just increased above the data point
+                        last_soc = soc_percent.get(minute - 1, 0)
                         if (
                             not discharge
-                            and soc_percent.get(minute - 1, 0) == (data_point + 1)
+                            and last_soc > data_point
                             and soc_percent.get(minute, 0) == data_point
                             and predbat_status.get(minute - 1, "") == "Charging"
                             and predbat_status.get(minute, "") == "Charging"
@@ -653,7 +654,7 @@ class Inverter:
                             and battery_power.get(minute, 0) < 0
                         ) or (
                             discharge
-                            and soc_percent.get(minute - 1, 0) == (data_point - 1)
+                            and last_soc < data_point
                             and soc_percent.get(minute, 0) == data_point
                             and predbat_status.get(minute - 1, "") in ["Exporting", "Discharging"]
                             and predbat_status.get(minute, "") in ["Exporting", "Discharging"]
@@ -685,7 +686,7 @@ class Inverter:
                                     from_soc = soc_kwh.get(minute, 0)
                                     to_soc = soc_kwh.get(target_minute, 0)
                                     soc_charged = from_soc - to_soc
-                                    average_power = total_power / total_count
+                                    average_power = total_power / total_count / max(abs(this_soc - last_soc), 1)
                                     charge_curve = round(min(average_power / max_power / self.base.battery_loss, 1.0), 2)
                                     if self.base.debug_enable:
                                         self.log(
@@ -701,9 +702,9 @@ class Inverter:
                                         )
                                     # Store data points
                                     if discharge:
-                                        store_range = range(this_soc, data_point - 1, -1)
+                                        store_range = range(this_soc, last_soc, -1)
                                     else:
-                                        store_range = range(this_soc, data_point + 1)
+                                        store_range = range(this_soc, last_soc)
 
                                     for point in store_range:
                                         if point not in final_curve:
