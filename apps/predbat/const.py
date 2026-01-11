9d92ffb83b245c237a7393156590268cb5dc9991
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2026-01-11 20:32:01 +0000

    Move to 2 minute updates (#3222)

--- apps/predbat/const.py
+++ apps/predbat/const.py
@@ -25,7 +25,7 @@ TIMEOUT = 60 * 5
 CONFIG_REFRESH_PERIOD = 60 * 8
 INVERTER_MAX_RETRY = 10  # Maximum number of retries for inverter commands
 INVERTER_MAX_RETRY_REST = 5  # Maximum number of retries for inverter REST commands
-INVERTER_QUICK_UPDATE_SECONDS = 60  # Minimum seconds between quick inverter data updates
+INVERTER_QUICK_UPDATE_SECONDS = 120  # Minimum seconds between quick inverter data updates
 
 # 240v x 100 amps x 3 phases / 1000 to kW / 60 minutes in an hour is the maximum kWh in a 1 minute period
 MAX_INCREMENT = 240 * 100 * 3 / 1000 / 60
