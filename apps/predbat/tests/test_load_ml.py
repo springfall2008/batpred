f9ac28f84e3e8821270caf108d44d302f3bf9129
Trefor Southwell <tdlj@tdlj.net>
2026-02-08 17:13:32 +0000

    Copyright date

--- apps/predbat/tests/test_load_ml.py
+++ apps/predbat/tests/test_load_ml.py
@@ -1,6 +1,6 @@
 # -----------------------------------------------------------------------------
 # Predbat Home Battery System
-# Copyright Trefor Southwell 2025 - All Rights Reserved
+# Copyright Trefor Southwell 2026 - All Rights Reserved
 # This application maybe used for personal use only and not for commercial use
 # -----------------------------------------------------------------------------
 # fmt: off
