6cc11bda53ef079737d33e80d1bd4447cd9e12b1
Trefor Southwell <48591903+springfall2008@users.noreply.github.com>
2026-06-06 19:31:15 +0100

    Update version to v8.40.0

--- apps/predbat/predbat.py
+++ apps/predbat/predbat.py
@@ -36,7 +36,7 @@ import pytz
 import requests
 import asyncio
 
-THIS_VERSION = "v8.39.16"
+THIS_VERSION = "v8.40.0"
 
 from download import predbat_update_move, predbat_update_download, check_install, resolve_predbat_repository, DEFAULT_PREDBAT_REPOSITORY
 from const import MINUTE_WATT
