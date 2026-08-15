54447ff7766edfd9f61e77f4e08b55f6acedc6da
Geoffrey Coan <142018870+gcoan@users.noreply.github.com>
2026-08-15 19:05:12 +0000

    revert checking octopus saving session join response as service currently doesn't include any response

--- apps/predbat/octopus.py
+++ apps/predbat/octopus.py
@@ -2974,7 +2974,13 @@ class Octopus:
                             else:
                                 # Join via octopus event (Bottle Cap Dave)
                                 cmd = "octopus_energy/join_octoplus_saving_session_event, event_code={}, entity_id={}".format(code, entity_id)
-                                result = self.call_service_wrapper("octopus_energy/join_octoplus_saving_session_event", event_code=code, entity_id=entity_id, return_response=True)
+                                # result = self.call_service_wrapper("octopus_energy/join_octoplus_saving_session_event", event_code=code, entity_id=entity_id, return_response=True)
+                                # 
+                                # at present bottle cap dave integration doesn't return a response from the saving session so can't check it succeeds
+                                # see https://github.com/BottlecapDave/HomeAssistant-OctopusEnergy/issues/1823
+                                # so for now dummy a positive response
+                                result = True
+                                self.call_service_wrapper("octopus_energy/join_octoplus_saving_session_event", event_code=code, entity_id=entity_id, return_response=True)
                             if result:
                                 if self.get_arg("set_event_notify"):
                                     msg = "Joined Octopus saving event " + start_time.strftime("%a %d/%m %H:%M") + "-" + end_time.strftime("%H:%M") + ", " + str(saving_rate) + " p/kWh"
