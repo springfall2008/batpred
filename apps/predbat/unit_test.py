534ea77e5603f4d52a63a9b3ab8527d1090e96fc
Robin <robin.holligan@gmail.com>
2026-08-19 15:07:17 +0100

    test: ensure UTF-8 console and LF file compatibility in test runners

--- apps/predbat/unit_test.py
+++ apps/predbat/unit_test.py
@@ -14,6 +14,11 @@ import sys
 import glob
 import argparse
 
+if hasattr(sys.stdout, "reconfigure"):
+    sys.stdout.reconfigure(encoding="utf-8", errors="replace")
+if hasattr(sys.stderr, "reconfigure"):
+    sys.stderr.reconfigure(encoding="utf-8", errors="replace")
+
 from predbat import PredBat
 from tests.test_infra import TestHAInterface
 from tests.test_compute_metric import run_compute_metric_tests
