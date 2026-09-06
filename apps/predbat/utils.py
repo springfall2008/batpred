3056725fe56a3e40d66a13b53160560f96d39fb6
Robin <robin.holligan@gmail.com>
2026-09-06 10:14:12 +0100

    fix(utils): catch TypeError in _libc_function for Windows ctypes compatibility

--- apps/predbat/utils.py
+++ apps/predbat/utils.py
@@ -1947,7 +1947,7 @@ def _libc_function(name, argtypes, restype):
     """
     try:
         function = getattr(ctypes.CDLL(None), name)
-    except (OSError, AttributeError):
+    except (OSError, AttributeError, TypeError):
         return None
     function.argtypes = argtypes
     function.restype = restype
